d64ed25492f150f404f3cdc3b553df486dd9fcbc
Joe Lamb <joe.lamb@campbellsci.com>
2026-07-27 16:17:45 -0600

    move the ball

--- index.js
+++ index.js
@@ -2,23 +2,26 @@
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
-// draw a red square
-ctx.beginPath();
-ctx.rect(20, 40, 100, 100);
-ctx.fillStyle = 'orange';
-ctx.fill();
-ctx.closePath();
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+let dx = -1;
+let dy = -3;
 
-// draw a green circle
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
-ctx.fillStyle = 'green';
-ctx.fill();
-ctx.closePath();
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(x, y, 10, 0, Math.PI * 2);
+  ctx.fillStyle = '#0095DD';
+  ctx.fill();
+  ctx.closePath();
+}
 
-// draw a blue empty rectangle
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = 'rgb(0 0 255 / 50%)';
-ctx.stroke();
-ctx.closePath();
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  drawBall();
+
+  x += dx;
+  y += dy;
+}
+
+setInterval(draw, 10);
